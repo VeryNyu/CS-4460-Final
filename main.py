9cf79995501da70707450ba967f0d382680f0810
VeryNyu <117503119+VeryNyu@users.noreply.github.com>
2026-04-09 17:40:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ def main():
         play.run()
     except KeyboardInterrupt:
         play.end()
-    except ConnectionError:
-        play.end("Please initiate wizardlm2 by running \'ollama run wizardlm2\' within your terminal")
-    except ResponseError:()
-        play.end("Please install wizardlm2 by running \'ollama pull wizardlm2\' within your terminal")
 
 
 if __name__ == "__main__":
